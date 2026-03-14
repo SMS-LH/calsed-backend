6be41dd950bf5ce9262d3f8005dfa1bf17cd7f28
Mourtalla <mourtallasall2004@gmail.com>
2026-03-14 20:47:57 +0000

    Fix guest checkout 500 error

--- server.js
+++ server.js
@@ -49,7 +49,9 @@ app.use(compression());
 // Configuration CORS : Tu peux restreindre à ton FRONTEND_URL en production
 app.use(cors());
 
-app.use(express.json());
+// --- CORRECTION PAYLOAD (Articles longs & Images Base64) ---
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, 
